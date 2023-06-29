7ce4fa5aa529221fa570ab5ef425f314225eab6c
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-29 20:38:58 +0330

    add tweet route procedure added to TRPC...

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { exampleRouter } from "~/server/api/routers/example";
+import { tweetRouter } from "~/server/api/routers/tweet";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  example: tweetRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/tweet.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+export const tweetRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(z.object({ content: z.string() }))
+    .mutation(async ({ input: { content }, ctx }) => {
+      const tweet = await ctx.prisma.tweet.create({
+        data: {
+          content,
+          userId: ctx.session.user.id,
+        },
+      });
+
+      return tweet;
+    }),
+});
