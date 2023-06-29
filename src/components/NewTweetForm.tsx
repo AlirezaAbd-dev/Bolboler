d4ec35deaa72641d07f928907b0fe0d0f6981331
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-29 20:09:53 +0330

    input bug fixed...

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,9 +1,26 @@
 import { useSession } from "next-auth/react";
 import Button from "./Button";
 import ProfileImage from "./ProfileImage";
+import { useCallback, useLayoutEffect, useRef, useState } from "react";
+
+function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
+  if (textArea == null) return;
+  textArea.style.height = "0";
+  textArea.style.height = `${textArea.scrollHeight}px`;
+}
 
 const NewTweetForm = () => {
   const session = useSession();
+  const [inputValue, setInputValue] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
+
+  useLayoutEffect(() => {
+    updateTextAreaSize(textAreaRef.current);
+  }, [inputValue]);
 
   if (session.status !== "authenticated") return;
 
@@ -12,6 +29,11 @@ const NewTweetForm = () => {
       <div className="flex gap-4">
         <ProfileImage src={session.data.user.image} />
         <textarea
+          ref={inputRef}
+          style={{ height: 0 }}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
           className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
           placeholder="What's happening?"
         ></textarea>
