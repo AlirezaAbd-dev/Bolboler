d5ac5565991c577baef228f4d33979fe7f64b34c
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-30 20:51:49 +0330

    tweets card created...

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,3 +1,8 @@
+import Link from "next/link";
+import InfiniteScroll from "react-infinite-scroll-component";
+import ProfileImage from "./ProfileImage";
+import { useSession } from "next-auth/react";
+
 type Tweet = {
   id: string;
   content: string;
@@ -15,8 +20,78 @@ type InfiniteTweetListProps = {
   tweets: Tweet[];
 };
 
-const InfiniteTweetList = ({ tweets }: InfiniteTweetListProps) => {
-  return <div>InfinitTweetList</div>;
+const InfiniteTweetList = ({
+  tweets,
+  isError,
+  isLoading,
+  fetchNewTweets,
+  hasMore,
+}: InfiniteTweetListProps) => {
+  if (isLoading) return <h1>Loading...</h1>;
+  if (isError) return <h1>Error...</h1>;
+
+  if (!tweets || tweets.length === 0) {
+    return (
+      <h2 className="my-4 text-center text-2xl text-gray-500">No Tweets</h2>
+    );
+  }
+
+  return (
+    <ul>
+      <InfiniteScroll
+        dataLength={tweets.length}
+        next={fetchNewTweets}
+        hasMore={hasMore}
+        loader={"Loading..."}
+      >
+        {tweets.map((tweet) => (
+          <TweetCard key={tweet.id} {...tweet} />
+        ))}
+      </InfiniteScroll>
+    </ul>
+  );
 };
+
+const dateTimeFormatter = new Intl.DateTimeFormat("fa", {
+  dateStyle: "short",
+});
+
+function TweetCard({
+  id,
+  user,
+  content,
+  createdAt,
+  likeCount,
+  likedByMe,
+}: Tweet) {
+  return (
+    <li className="flex gap-4 border-b p-4">
+      <Link href={`/profiles/${user.id}`}>
+        <ProfileImage src={user.image} />
+      </Link>
+      <div className="flex flex-grow flex-col">
+        <div className="flex gap-1">
+          <Link
+            href={`/profiles/${user.id}`}
+            className="font-bold outline-none hover:underline focus-visible:underline"
+          >
+            {user.name}
+          </Link>
+          <span className="text-gray-500">-</span>
+          <span className="text-gray-500">
+            {dateTimeFormatter.format(createdAt)}
+          </span>
+        </div>
+        <p className="whitespace-pre-wrap">{content}</p>
+        <HeartButton />
+      </div>
+    </li>
+  );
+}
+
+function HeartButton() {
+  const session = useSession();
+  return <h1>Heart</h1>;
+}
 
 export default InfiniteTweetList;
