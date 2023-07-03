ec312743284e05db34fb5394a694441751d929c7
0AliReza0 <alireza.abedi9310@gmail.com>
2023-07-04 00:14:57 +0330

    tweet time style changed...

--- src/components/card/Content.tsx
+++ src/components/card/Content.tsx
@@ -117,7 +117,12 @@ const Content = (props: ContentProps) => {
 };
 
 const dateTimeFormatter = new Intl.DateTimeFormat(undefined, {
-  dateStyle: "medium",
+  minute: "2-digit",
+  hour: "2-digit",
+  second: "2-digit",
+  year: "numeric",
+  month: "short",
+  day: "2-digit",
 });
 
 export default Content;
