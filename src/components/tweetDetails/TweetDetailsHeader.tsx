86204ea5c9a3a7862547a79730462df40593a98a
0AliReza0 <alireza.abedi9310@gmail.com>
2023-07-24 19:09:12 +0330

    tweet details page header has dark mode now...

--- src/components/tweetDetails/TweetDetailsHeader.tsx
+++ src/components/tweetDetails/TweetDetailsHeader.tsx
@@ -5,14 +5,16 @@ import { IconHoverEffect } from '../IconHoverEffect';
 
 const TweetDetailsHeader = () => {
     return (
-        <header className="sticky top-0 z-10 flex items-center border-b bg-white px-4 py-2">
+        <header className="sticky top-0 z-10 flex items-center border-b dark:border-gray-800 bg-white dark:bg-gray-900 px-4 py-2">
             <Link href=".." className="mr-2">
                 <IconHoverEffect>
-                    <VscArrowLeft className="h-6 w-6" />
+                    <VscArrowLeft className="h-6 w-6 dark:fill-white" />
                 </IconHoverEffect>
             </Link>
             <div className="ml-2 flex-grow">
-                <h1 className="text-lg font-bold">Tweet Details</h1>
+                <h1 className="text-lg font-bold dark:text-white">
+                    Tweet Details
+                </h1>
             </div>
         </header>
     );
