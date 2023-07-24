669194baf8001650021af4e3e00ab867b8ddd690
0AliReza0 <alireza.abedi9310@gmail.com>
2023-07-24 19:31:33 +0330

    change theme button has been responsived...

--- src/components/ui/ChangeThemeButton.tsx
+++ src/components/ui/ChangeThemeButton.tsx
@@ -13,14 +13,14 @@ const ChangeThemeButton = () => {
                 if (theme === 'dark') setTheme('light');
                 else setTheme('dark');
             }}
-            className={`fixed cursor-pointer bottom-4 left-4 p-3 rounded-full ${
+            className={`fixed cursor-pointer bottom-4 left-4 p-2 md:p-3 rounded-full ${
                 theme === 'dark' ? 'hover:bg-gray-800' : 'hover:bg-gray-200'
             }`}
         >
             {theme === 'dark' ? (
-                <BsFillMoonStarsFill className="w-6 h-6 fill-white" />
+                <BsFillMoonStarsFill className="w-3 h-3 md:w-6 md:h-6 fill-white" />
             ) : (
-                <BiSolidSun className="w-6 h-6 fill-gray-90" />
+                <BiSolidSun className="w-3 h-3 md:w-6 md:h-6 fill-gray-90" />
             )}
         </span>
     );
