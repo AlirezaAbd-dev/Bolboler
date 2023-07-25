5106cb2910fea59166ef14f2b1da7f4c96e7419e
0AliReza0 <alireza.abedi9310@gmail.com>
2023-07-25 12:55:09 +0330

    with changing the system's theme, bolboler's theme also change...

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -8,6 +8,7 @@ export const ThemeContext = createContext<ThemeContextType | null>(null);
 
 const ThemeContextProvider = (props: { children: React.ReactNode }) => {
     const [theme, setTheme] = useState<ThemeType>('dark');
+    const [isWindow, setIsWindow] = useState(false);
 
     useEffect(() => {
         setTheme(
@@ -15,10 +16,19 @@ const ThemeContextProvider = (props: { children: React.ReactNode }) => {
                 ? 'dark'
                 : 'light',
         );
+        setIsWindow(true);
     }, []);
 
     function changeTheme(theme: ThemeType) {
         setTheme(theme);
+    }
+
+    if (isWindow) {
+        window
+            .matchMedia('(prefers-color-scheme: dark)')
+            .addEventListener('change', function () {
+                this.matches ? setTheme('dark') : setTheme('light');
+            });
     }
 
     return (
